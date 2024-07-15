ac985feb8a1d333abd7255dc5164efaa212e41fd
mac <singh.ravneet1995@gmail.com>
2024-07-15 19:41:53 +0530

    updated login code

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -5,3 +5,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='myapp/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
+
+
